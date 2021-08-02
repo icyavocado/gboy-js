fb6c2374c3a0cc48c726d8c07de4ba78b697f605
Dai <dainsait@gmail.com>
2021-08-02 12:46:49 -0600

    UPDATE ram and cpu to use uint8 and uint16 for wrapping over protection

--- src/core/lr35902/lr35902.cpu.js
+++ src/core/lr35902/lr35902.cpu.js
@@ -12,13 +12,17 @@ export default class LR35902 {
     this.cycles = 0x00;
 
     // Registers
-    this.A = 0x00;
-    this.B = 0x00;
-    this.C = 0x00;
-    this.D = 0x00;
-    this.E = 0x00;
-    this.H = 0x00;
-    this.L = 0x00;
+    this.r8 = new Uint8Array(
+      [0, 0, 0, 0, 0, 0, 0, 0]
+    );
+    this.r16 = new Uint16Array([0]);
+    this._A = this.r8[0];
+    this._B = this.r8[1];
+    this._C = this.r8[2];
+    this._D = this.r8[3];
+    this._E = this.r8[4];
+    this._H = this.r8[5];
+    this._L = this.r8[6];
 
     // Flag Registers
     this.z = 0x00;
@@ -47,48 +51,60 @@ export default class LR35902 {
     return this.RAM[address];
   }
   write(address, value) {
-    this.RAM[address] = value;
-    this.CPU.pc++;
+    this.RAM.write(address, value);
+    this.pc++;
     this.cycles += 2;
   }
-  get F() {
-    return (
-      ((this.z & 0xff) << 8) |
-      ((this.n & 0xff) << 7) |
-      ((this.h & 0xff) << 6) |
-      ((this.c & 0xff) << 5)
-    );
-  }
-  set F(value = 0) {
-    this.z = value << 8;
-    this.n = value << 7;
-    this.h = value << 6;
-    this.c = value << 5;
-    this.cycles += 4;
-  }
+  /**
+   * These get and set for single register is to make sure
+   * we have wrapover protection
+   */
+  get A() { return this._A; }
+  get B() { return this._B; }
+  get C() { return this._C; }
+  get D() { return this._D; }
+  get E() { return this._E; }
+  get F() { return this._F; }
+  get H() { return this._H; }
+  get L() { return this._L; }
+  set A(value) { this.r8[0] = value }
+  set B(value) { this.r8[1] = value }
+  set C(value) { this.r8[2] = value }
+  set D(value) { this.r8[3] = value }
+  set E(value) { this.r8[4] = value }
+  set H(value) { this.r8[5] = value }
+  set L(value) { this.r8[6] = value }
+  /**
+   * F can't be set via set F(),
+   * but we can set the flags individually
+   */
+  get F() { this.F = this.z << 7 | this.n << 6 | this.h << 5 | this.c << 4; }
+  /**
+   * Get and set the 16 bit version of registers combine
+   */
   get AF() {
-    return (this.A & (0xff << 8)) | (this.F & 0xff);
+    return this._get("AF");
   }
   set AF(value) {
-    return this._set("AF", value);
+    return this._set16bit("AF", value);
   }
   get BC() {
-    return ((this.B & 0xff) << 8) | (this.C & 0xff);
+    return this._get("BC");
   }
   set BC(value) {
-    return this._set("BC", value);
+    return this._set16bit("BC", value);
   }
   get DE() {
-    return ((this.D & 0xff) << 8) | (this.E & 0xff);
+    return this._get("DE");
   }
   set DE(value) {
-    return this._set("DE", value);
+    return this._set16bit("DE", value);
   }
   get HL() {
-    return ((this.H & 0xff) << 8) | (this.L & 0xff);
+    return this._get("HL");
   }
   set HL(value) {
-    return this._set("HL", value);
+    return this._set16bit("HL", value);
   }
   get SP() {
     return this.sp;
@@ -96,33 +112,33 @@ export default class LR35902 {
   set SP(value) {
     this.sp = value;
   }
-  // Get immediate 8 bit
+  /**
+   * Get immediate 8 bit and 8 bit signed
+   */
   get _n() {
     return this.read();
   }
-  // Get immediate 16 bit
+  get _n_signed() {
+    return ~this._n + 1;
+  }
+  /**
+   * Get immediate 16 bit and 16 bit signed
+   */
   get _nn() {
     return (this.read() & 0xff) << 8 | (this.read() & 0xff);
   }
-  _set(registers, value) {
-    if (value > 255) {
-      this._set16bit(registers, value);
-    } else {
-      this._set8bit(
-        {
-          high: registers[0],
-          low: registers[1]
-        },
-        value
-      );
-    }
+  get _nn_signed() {
+    return ~this._nn() + 1;
   }
-  _set8bit(register, value) {
-    this[register] = value;
-    this.cycles += 4;
+  /**
+   * Helpers
+   */
+  _get(registers) {
+    this.r16[0] = (this[registers[0]] & 0xff) << 8 | (this[registers[1]] & 0xff);
+    return this.r16[0];
   }
-  _set16bit({ high, low }, value) {
-    this._set8bit(high, value >> 8);
-    this._set8bit(low, value & 0xff);
+  _set16bit(registers, value) {
+    this[registers[0]] = value >> 8;
+    this[registers[1]] = value & 0xff;
   }
 }
